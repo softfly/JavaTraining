2d5e150f9bd2651e8e5032b19afbddd2f50a4e8b
Grzegorz Ziemski <ziemski@softfly.pl>
2018-08-20 14:57:16 +0200

    NetBeans 8.2 -> New Project -> Maven -> Enterprise Application +IntelliJ format

---
+++ EJBTraining/EJBTraining-ejb/src/main/java/training/javaee/NewSessionBean.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package training.javaee;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * @author martin
+ */
+@Stateless
+@LocalBean
+public class NewSessionBean {
+
+    public void businessMethod() {
+    }
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+}
