5e58efcfefef8c51118cb9f850bca3760ad6d9c0
Grzegorz Ziemski <ziemski@softfly.pl>
2018-08-22 12:33:30 +0200

    https://github.com/javaee/tutorial-examples/tree/master/ejb/cart Merge +IntelliJ format

---
+++ EJBTraining/EJBTraining-common/src/main/java/javaeetutorial/cart/ejb/Cart.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2014 Oracle and/or its affiliates. All rights reserved.
+ * <p>
+ * You may not modify, use, reproduce, or distribute this software except in
+ * compliance with  the terms of the License at:
+ * https://github.com/javaee/tutorial-examples/LICENSE.txt
+ */
+package javaeetutorial.cart.ejb;
+
+
+import javaeetutorial.cart.util.BookException;
+
+import javax.ejb.Remote;
+import java.util.List;
+
+
+@Remote
+public interface Cart {
+    public void initialize(String person) throws BookException;
+
+    public void initialize(
+            String person,
+            String id) throws BookException;
+
+    public void addBook(String title);
+
+    public void removeBook(String title) throws BookException;
+
+    public List<String> getContents();
+
+    public void remove();
+}
